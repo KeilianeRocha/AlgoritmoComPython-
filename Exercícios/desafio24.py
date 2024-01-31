450af1135f5290cd380d9e21228dcb56923e4a19
KeilianeRocha <keilianesrocha>
2024-01-31 00:08:25 -0300

    exercicios 23 e 24

--- Exercícios/desafio24.py
+++ Exercícios/desafio24.py
@@ -7,3 +7,15 @@ dezena: 3
 centena: 8
 milhar: 1
 """
+import math
+
+numero = int(input('Digite um número: '))
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+print('Analisando o número {}'.format(numero))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
