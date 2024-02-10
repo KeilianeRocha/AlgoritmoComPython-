548248fb566400267eb492baff3bf2d8ab417b8c
KeilianeRocha <keilianesrocha>
2024-02-10 01:59:59 -0300

    Exercício28

--- Exercícios/desafio28.py
+++ Exercícios/desafio28.py
@@ -7,3 +7,8 @@ Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o
  último = Souza
 
 """
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Seu primeiro nome é {}'.format(nome[0]).title())
+print('O seu último nome é {}'.format(nome[len(nome)-1]).title())
+
