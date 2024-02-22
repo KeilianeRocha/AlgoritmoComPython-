5fd7fd1342459cf2f2307aab3e089b4b94eb7355
KeilianeRocha <keilianesrocha>
2024-02-21 21:00:58 -0300

    .gitignore

--- Exercícios/buscadorNome.py
+++
@@ -1,9 +0,0 @@
-"""
-Crie um programa que leia o nome de uma pessoa e diga se ele tem "Silva" no nome.
-"""
-name = str(input('Qual é o seu nome? ')).title().strip()
-print('Seu nome tem Silva? {}'.format('Silva' in name))
-
-
-
-
--- Exercícios/buscadorNomeCidade.py
+++
@@ -1,5 +0,0 @@
-"""
-Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo".
-"""
-town = str(input('Em que cidade você nasceu? ')).upper().strip()
-print(town[:5] == 'SANTO')
